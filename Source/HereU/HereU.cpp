606d0d24e6dd234718d9ed79e3884da4767cb960
AlexUlit <aleksandrulitnikov@gmail.com>
2022-09-11 09:52:29 +0200

    HereU-01 #Project initialization

---
+++ Source/HereU/HereU.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HereU.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HereU, "HereU" );
---
+++ Source/HereU/HereUGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HereUGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEREU_API AHereUGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
